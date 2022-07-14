ea0c4646564a379d0024f0a9719abab20d36dbb9
Younghun Kwon <kyonggi@kakao.com>
2022-07-14 18:22:50 +0900

    기본 파일 설치

---
+++ src/main/java/com/kwon99/turtleat/TurtleatApplication.java
@@ -0,0 +1,13 @@
+package com.kwon99.turtleat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurtleatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurtleatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kwon99/turtleat/TurtleatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwon99.turtleat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurtleatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
