b028239d10081cbd688de87fe45f196c45b08c1f
Younghun Kwon <kyonggi@kakao.com>
2022-07-19 16:45:44 +0900

    [#12] 등록 페이지 템플릿 삽입 및 필요한 값 정제

--- src/main/java/com/kwon99/turtleat/controller/ThymeleafController.java
+++ src/main/java/com/kwon99/turtleat/controller/ThymeleafController.java
@@ -1,10 +1,9 @@
 package com.kwon99.turtleat.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import com.kwon99.turtleat.service.Test;
 
 @Controller
 public class ThymeleafController {
@@ -21,6 +20,17 @@ public class ThymeleafController {
         return "map";
     }
 
+
+    @GetMapping("/test")
+    public String test(Test test, Model model) {
+        System.out.println(test.getValue());
+        return "test";
+    }
+
+    // register
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
 }
-
-
